2a99c0912e0898ca283f0893db5547159b8bd284
Scott Frees <scott.frees@gmail.com>
2015-07-09 12:22:02 -0400

    recording test

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var should = require('chai').should();
+var sinon = require('sinon');
 var recorder = require('../index');
 
 var r = new recorder();
@@ -19,4 +20,24 @@ describe ('recorder', function() {
     	r1.track({name:'test'});
     	r1.tracked().length.should.equal(1);
     })
+});
+
+describe('recorder samples', function() {
+    var clock;
+    beforeEach(function() {
+        clock = sinon.useFakeTimers();
+    });
+    afterEach(function() {
+        clock = clock.restore();
+    });
+    it ("should record one sample per interval", function() {
+        r = new recorder();
+        r.interval = 50;
+        r.track({name:'test', 
+            position: {x:0, y:0, z:0}, 
+            orientation:{x:0, y:0, z:0, w:1 }});
+        r.start()
+        clock.tick(r.interval+1);
+        r.samples.length.should.equal(2); // once on start, once after 50ms
+    });
 })

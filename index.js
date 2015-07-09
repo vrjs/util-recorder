85f4e105ae0fef10d201f1b3f858800f099ce4ac
= <scott.frees@gmail.com>
2015-07-09 16:08:11 -0400

    sample querying tests

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function () {
     this.interval = 50;
     this.activated = false;
     this.internal_samples = [];
+    this.sequence_number = 0;
     this.active = function () {
     	return this.activated;
     }, 
@@ -13,16 +14,21 @@ module.exports = function () {
     }, 
     this.track = function (obj) {
     	this.objects.push(obj);
-    	console.log(this.objects.length)
     }
     this.start = function () {
         this.activated = true;
         record_now(this);
     }
-    this.samples = function() {
-        return this.internal_samples;
+    this.samples = function(query, callback, num_samples) {
+        if (num_samples) {
+            query.sequence_number = {'$gte':this.sequence_number-num_samples}
+        }
+        this.data.find(query, function (err, val){
+            callback(err, val);
+        });
     }
     this.record_sample = function() {
+        var time_stamp = Date.now();
         // id
         // name
         // local position
@@ -30,9 +36,21 @@ module.exports = function () {
         // world position
         // world orientation (quaternion)
         // current time
-        // sample id
+        // sequence_number
+        for (var i in this.objects) {
+            var o = this.objects[i];
+            var sample = {
+                id : o.id,
+                name : o.name,
+                local_position : o.position, 
+                local_orientation : o.quaternion, 
+                time : time_stamp, 
+                sequence_number : this.sequence_number
+            }
+            this.data.insert(sample);
+        }
 
-        this.internal_samples.push(this.objects[0]);
+        this.sequence_number++;
     }
 
     this.data = new Datastore({inMemoryOnly:true});
--- test/index.js
+++ test/index.js
@@ -4,12 +4,19 @@ var recorder = require('../index');
 
 var r = new recorder();
 
-var pando = {
+var pando1 = {
     id : 0,
     name:'test', 
     position: {x:0, y:0, z:0}, 
     quaternion:{x:0, y:0, z:0, w:1 }
 };
+var pando2 = {
+    id : 0,
+    name:'test2', 
+    position: {x:0, y:0, z:0}, 
+    quaternion:{x:0, y:0, z:0, w:1 }
+};
+
 
 describe ('recorder', function() {
     it ('returns false for active if not activated', function() {
@@ -31,26 +38,74 @@ describe ('recorder', function() {
 
 describe('recorder samples', function() {
     var clock;
-    beforeEach(function() {
+    it ("should record one sample per interval", function(done) {
         clock = sinon.useFakeTimers();
-    });
-    afterEach(function() {
-        clock = clock.restore();
-    });
-    it ("should record one sample per interval", function() {
         r = new recorder();
         r.interval = 50;
-        r.track(pando);
+        r.track(pando1);
         r.start()
         clock.tick(r.interval);
-        r.samples().length.should.equal(2); // once on start, once after 50ms
+        clock = clock.restore();
+        r.samples({}, function(err, samples){
+          samples.length.should.equal(2);
+          done();   
+        }) // once on start, once after 50ms
     });
-    it ("should record 4 samples after three intervals", function() {
+    it ("should record 4 samples after three intervals", function(done) {
+        clock = sinon.useFakeTimers();
         r = new recorder();
         r.interval = 10;
-        r.track(pando);
+        r.track(pando1);
         r.start()
         clock.tick(r.interval*3);
-        r.samples().length.should.equal(4); 
+        clock = clock.restore();
+        r.samples({}, function(err, samples){
+          samples.length.should.equal(4);   
+          done();
+        })
+    });
+
+    it ("should record 8 samples after three intervals if two objects are tracked", function(done) {
+        clock = sinon.useFakeTimers();
+        r = new recorder();
+        r.interval = 10;
+        r.track(pando1);
+        r.track(pando2);
+        r.start()
+        clock.tick(r.interval*3);
+        clock = clock.restore();
+        r.samples({}, function(err, samples){
+          samples.length.should.equal(8); 
+          done();  
+        })
+    });
+
+    it ("should record 4 samples after three intervals for specific object", function(done) {
+        clock = sinon.useFakeTimers();
+        r = new recorder();
+        r.interval = 10;
+        r.track(pando1);
+        r.track(pando2);
+        r.start()
+        clock.tick(r.interval*3);
+        clock = clock.restore();
+        r.samples({'name':'test'}, function(err, samples){
+          samples.length.should.equal(4);   
+          done()
+        })
+    });
+    it ("should record 2 samples after three intervals for specific object if only 2 were requested", function(done) {
+        clock = sinon.useFakeTimers();
+        r = new recorder();
+        r.interval = 10;
+        r.track(pando1);
+        r.track(pando2);
+        r.start()
+        clock.tick(r.interval*3);
+        clock = clock.restore();
+        r.samples({'name':'test'}, function(err, samples){
+          samples.length.should.equal(2);   
+          done();
+        }, 2)
     });
 })
